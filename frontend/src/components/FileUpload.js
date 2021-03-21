a830aab58ecb36d788a179023ef71f2412e74719
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-03-21 16:38:13 -0400

    image display component

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
-  const [fileName, setFileName] = useState('Choose File');
+  const [fileName, setFileName] = useState('');
   const [uploadedFile, setUploadedFile] = useState({});
 
   const onChange = (e) => {
@@ -37,7 +37,9 @@ const FileUpload = () => {
     <Fragment>
       <form onSubmit={onSubmit}>
         <div className='formfile mb-4'>
-          <label htmlFor='formFile' className='form-label'></label>
+          <label htmlFor='formFile' className='form-label'>
+            {fileName}
+          </label>
           <input
             className='form-control'
             type='file'
@@ -53,6 +55,14 @@ const FileUpload = () => {
           </div>
         </div>
       </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+          </div>
+        </div>
+      ) : null}
     </Fragment>
   );
 };
