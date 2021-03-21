710981689a0d063ed588e44ed1afd48274cd089b
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-03-21 16:03:17 -0400

    state management and uploading image to folder

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,12 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log('There is server problem');
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
+
   return (
     <Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='formfile mb-4'>
           <label htmlFor='formFile' className='form-label'></label>
-          <input className='form-control' type='file' id='formFile' />
+          <input
+            className='form-control'
+            type='file'
+            id='formFile'
+            onChange={onChange}
+          />
           <div className='d-grid gap-2'>
             <input
               type='submit'
